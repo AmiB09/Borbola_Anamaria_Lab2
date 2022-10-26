300f2870dba6e74a85eb7982a56926dc67ebbc5d
Ami <Ami@DESKTOP-7LGIB2I>
2022-10-26 15:44:36 +0300

    Add project files.

---
+++ Data/Borbola_Anamaria_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Borbola_Anamaria_Lab2.Models;
+
+namespace Borbola_Anamaria_Lab2.Data
+{
+    public class Borbola_Anamaria_Lab2Context : DbContext
+    {
+        public Borbola_Anamaria_Lab2Context (DbContextOptions<Borbola_Anamaria_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Borbola_Anamaria_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Borbola_Anamaria_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Borbola_Anamaria_Lab2.Models.Author> Author { get; set; }
+    }
+}
